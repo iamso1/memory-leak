37c1d260e3ba78149d4799fdf1829488befd0c4e
Hsinhan <bmhiamso1@gmail.com>
2019-02-26 16:36:10 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var net = require('net');
+const schedule = require('node-schedule');
+const moment = require('moment');
+
+var server = net.createServer(function(socket) {
+  console.log('wawaji in');
+  socket.on('data', function(data) {
+    socket.write(data);
+  });
+  socket.on('error', function() {
+    console.log('wawa socket error, lose connection');
+  });
+});
+
+schedule.scheduleJob('* */1 * * *', function() {
+  const memory = process.memoryUsage();
+  console.log(
+    moment().format('YYYY/MM/DD HH:mm:ss'),
+    'rss',
+    memory.rss / (1024 * 1024),
+    'heapTotal',
+    memory.heapTotal / (1024 * 1024),
+    'heapUsed',
+    memory.heapUsed / (1024 * 1024),
+    'external',
+    memory.external / (1024 * 1024),
+  );
+});
+server.listen(3000, 'localhost');
---
+++ test/client.js
@@ -0,0 +1,40 @@
+//模擬娃娃機連線行為
+var net = require('net');
+var conn;
+var quitting = false;
+var timer;
+
+(function connect() {
+  function reconnect() {
+    setTimeout(connect, 5000);
+  }
+
+  conn = net.createConnection(3000, 'localhost');
+
+  conn.on('connect', function() {
+    console.log('connect to server');
+
+    timer = setInterval(() => {
+      conn.write(
+        Buffer.from([0, 0, 0, 0, 0, 0, 0, 53, 65, 65, 65, 65, 65, 65, 49, 49, 49, 49, 49, 49]),
+      );
+    }, 1000);
+  });
+
+  conn.on('data', function(data) {
+    console.log(data);
+    console.log('wawaji receive data from server:', data);
+  });
+
+  conn.on('error', function(err) {
+    console.log('Error in connection:', err);
+  });
+
+  conn.on('close', function() {
+    clearInterval(timer);
+    if (!quitting) {
+      console.log('connection got closed, will try to reconnect');
+      reconnect();
+    }
+  });
+})();
